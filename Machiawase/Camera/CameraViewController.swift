715e569a8ced5466c89c3be9cb17f02501c91c77
Kohei Niimi <kouhei.aquamarines@gmail.com>
2017-03-04 16:27:42 +0900

    add parse firebase

--- Machiawase/Camera/CameraViewController.swift
+++ Machiawase/Camera/CameraViewController.swift
@@ -19,11 +19,20 @@ class CameraViewController: UIViewController, CLLocationManagerDelegate {
     private let ref = FIRDatabase.database().reference()
     private var firebaseName: String! = nil
     private var firebaseId: String! = nil
+    private var peoples: [People] = []
 
     struct MLocation {
         public var latitude: CLLocationDegrees!
         public var longitude: CLLocationDegrees!
         public var altitude: CLLocationDistance!
+    }
+    
+    struct People {
+        var identifier: String
+        var name: String
+        var latitude: Double
+        var longitude: Double
+        var altitude: Double
     }
     
     override func viewDidLoad() {
@@ -138,8 +147,8 @@ class CameraViewController: UIViewController, CLLocationManagerDelegate {
     /** upload my location data to firebase */
     private func uploadMyLocation(fromLocation fromLc: MLocation) {
         if let name: String = self.firebaseName {
-            let post = ["user": ["name": name], "location": ["longitude": fromLc.longitude, "latitude": fromLc.latitude, "altitude": fromLc.altitude]] as [String : Any]
             if let id: String = self.firebaseId {
+                let post = ["id": ["id" : id], "user": ["name": name], "location": ["longitude": fromLc.longitude, "latitude": fromLc.latitude, "altitude": fromLc.altitude]] as [String : Any]
                 let childUpdates = ["/users/\(id)/": post]
                 ref.updateChildValues(childUpdates)
             }
@@ -148,7 +157,43 @@ class CameraViewController: UIViewController, CLLocationManagerDelegate {
     
     /** observe firebase**/
     private func observeOtherLocation(snapshot: FIRDataSnapshot) {
-      //
+        guard let dic = snapshot.value as? Dictionary<String, AnyObject> else {
+            return
+        }
+        var peopleInfo: [People] = []
+        for data in dic {
+            var people: People = People.init(identifier: "", name: "", latitude: 0.0, longitude: 0.0, altitude: 0.0)
+            
+            let name = data.value["user"]
+            let location = data.value["location"]
+            let id = data.value["id"]
+            if let userName: [String : String] = name as! [String : String]? {
+                if let user: String = userName["name"] {
+                    people.name = user
+                }
+            }
+            if let userLocation: [String : Double] = location as! [String : Double]? {
+                if let alt: Double = userLocation["altitude"] {
+                    people.altitude = alt
+                }
+                if let lat: Double = userLocation["latitude"] {
+                    people.latitude = lat
+                }
+                if let lon: Double = userLocation["longitude"] {
+                    people.longitude = lon
+                }
+            }
+            if let userId: [String : String] = id as! [String : String]? {
+                if let identifier: String = userId["identifier"] {
+                    people.identifier = identifier
+                }
+            }
+            peopleInfo.append(people)
+        }
+        if !self.peoples.isEmpty {
+            self.peoples.removeAll()
+        }
+        self.peoples = peopleInfo
     }
-
+    
 }
